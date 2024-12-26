eef1efc624f773da2a2624afb5ed39ecfd63c7a4
prayagpomaje719 <prayagpomaje719@gmail.com>
2024-12-26 14:40:43 +0530

    new version 1.8

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-    return "My Python flask app v1.7"
+    return "My Python flask app v1.8"
 
 app.run(host="0.0.0.0", port=4000)
